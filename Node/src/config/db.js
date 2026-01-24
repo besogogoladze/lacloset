16bd7e22530058892526d1f4f8d55cd7b7498d69
besogogoladze <Gogoladzebeso8@gmail.com>
2026-01-24 23:46:31 +0100

    comment db

--- Node/src/config/db.js
+++ Node/src/config/db.js
@@ -1,27 +1,24 @@
-import mongoose from "mongoose";
-let isConnected = false;
-const connectDB = async () => {
-  if (isConnected) {
-    console.log("MongoDB already connected.");
-    return;
-  }
+// import mongoose from "mongoose";
+// let isConnected = false;
+// const connectDB = async () => {
+//   if (isConnected) {
+//     console.log("MongoDB already connected.");
+//     return;
+//   }
 
-  if (mongoose.connection.readyState === 1) {
-    isConnected = true;
-    console.log("MongoDB already connected (readyState = 1).");
-    return;
-  }
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    isConnected = conn.connections[0].readyState === 1;
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
-};
+//   if (mongoose.connection.readyState === 1) {
+//     isConnected = true;
+//     console.log("MongoDB already connected (readyState = 1).");
+//     return;
+//   }
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+//     isConnected = conn.connections[0].readyState === 1;
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error(`Error: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
 
-export default connectDB;
+// export default connectDB;
